fa4c723e8fd231732b40785e463f8e244de14e3e
q14781256 <59154265+q14781256@users.noreply.github.com>
2020-01-10 16:25:20 +0800

    Create ClientReprieveLose.java

---
+++ src/main/java/com/neusoft/entity/ClientReprieveLose.java
@@ -0,0 +1,35 @@
+package com.neusoft.entity;
+
+public class ClientReprieveLose {
+    private Integer clientReprieveLoseId;
+
+    private String reprieveStep;
+
+    private String clientCode;
+
+	public Integer getClientReprieveLoseId() {
+		return clientReprieveLoseId;
+	}
+
+	public void setClientReprieveLoseId(Integer clientReprieveLoseId) {
+		this.clientReprieveLoseId = clientReprieveLoseId;
+	}
+
+	public String getReprieveStep() {
+		return reprieveStep;
+	}
+
+	public void setReprieveStep(String reprieveStep) {
+		this.reprieveStep = reprieveStep;
+	}
+
+	public String getClientCode() {
+		return clientCode;
+	}
+
+	public void setClientCode(String clientCode) {
+		this.clientCode = clientCode;
+	}
+
+    
+}
