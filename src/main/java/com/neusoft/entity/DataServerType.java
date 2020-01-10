5ea2c342eb3b9096b1200ac66d0bfc96ee57855c
q14781256 <59154265+q14781256@users.noreply.github.com>
2020-01-10 16:25:29 +0800

    Create DataServerType.java

---
+++ src/main/java/com/neusoft/entity/DataServerType.java
@@ -0,0 +1,25 @@
+package com.neusoft.entity;
+
+public class DataServerType {
+    private Integer dstId;
+
+    private String dstName;
+
+	public Integer getDstId() {
+		return dstId;
+	}
+
+	public void setDstId(Integer dstId) {
+		this.dstId = dstId;
+	}
+
+	public String getDstName() {
+		return dstName;
+	}
+
+	public void setDstName(String dstName) {
+		this.dstName = dstName;
+	}
+
+    
+}
