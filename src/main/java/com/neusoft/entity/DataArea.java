b34d09c9605aac4509c320923100527d91d73c75
q14781256 <59154265+q14781256@users.noreply.github.com>
2020-01-10 16:25:25 +0800

    Create DataArea.java

---
+++ src/main/java/com/neusoft/entity/DataArea.java
@@ -0,0 +1,25 @@
+package com.neusoft.entity;
+
+public class DataArea {
+    private Integer daId;
+
+    private String daName;
+
+	public Integer getDaId() {
+		return daId;
+	}
+
+	public void setDaId(Integer daId) {
+		this.daId = daId;
+	}
+
+	public String getDaName() {
+		return daName;
+	}
+
+	public void setDaName(String daName) {
+		this.daName = daName;
+	}
+
+    
+}
