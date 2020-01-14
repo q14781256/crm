99f12f998586f763e1e0923aeef5e62f820d1223
Piansiheng <59648504+Piansiheng@users.noreply.github.com>
2020-01-14 16:15:51 +0800

    Create ClientReprieveLoseService.java

---
+++ src/main/java/com/neusoft/service/cust/crm/ClientReprieveLoseService.java
@@ -0,0 +1,12 @@
+package com.neusoft.service.cust.crm;
+
+import org.apache.ibatis.annotations.Update;
+
+import com.neusoft.entity.ClientReprieveLose;
+
+
+
+public interface ClientReprieveLoseService {
+	
+	public int updateClientReprieveLose(ClientReprieveLose clientReprieveLose);
+}
